519296e046e5b1e4c1e9ef1c742208eda3b7e693
Mike93222 <km9639@mci4me.at>
2025-12-09 10:29:21 +0100

    added normale Ansicht

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-
 import streamlit as st
-st.write("## Wartungskosten")
 
-# Callbacks for the number inputs
-def update_price_from_euro():
-    st.session_state.price_dollar = st.session_state.price_euro * 1.1
+if "sb_current_device" not in st.session_state:
+    st.session_state.sb_current_device = ""
 
-def update_price_from_dollar():
-    st.session_state.price_euro = st.session_state.price_dollar * 0.9
+# Eine Überschrift der ersten Ebene
+st.write("# Gerätemanagement")
 
-# Number inputs for the maintenance costs
-st.number_input(label="Wartungskosten in Euro", 
-                            key = "price_euro",
-                            on_change=update_price_from_euro)
+# Eine Überschrift der zweiten Ebene
+st.write("## Geräteauswahl")
 
-st.number_input(label="Wartungskosten in Dollar", 
-                            key = "price_dollar",
-                            on_change=update_price_from_dollar)
+# Eine Auswahlbox mit hard-gecoded Optionen, das Ergebnis
+
+st.session_state.sb_current_device = st.selectbox(label='Gerät auswählen',
+        options = ["Gerät_A", "Gerät_B"])
+
+st.write(f"Das ausgewählte Gerät ist {st.session_state.sb_current_device}")
